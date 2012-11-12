e310105596db14783242dd7d6aab81095120a7f5
Philip Guin <philipguin@gmail.com>
2012-11-11 22:35:34 -0600

    Minor change.

--- src/fitness/IFitnessed.java
+++ src/fitness/IFitnessed.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public interface IFitnessed
 {
 	/** Ideally, this should be as fast as possible-- this should behave as a field getter,
-         * given the frequency at which this method will be called by owners of this interface. */
+     * given the frequency at which this method will be called by owners of this interface. */
 	public float getFitness();
 
 	public static final Comparator<IFitnessed> fitnessComparator = new Comparator<IFitnessed>()
