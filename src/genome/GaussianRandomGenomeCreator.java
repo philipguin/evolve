69c95b76729dfd80d7c024cf5e7f22d489d65ac3
Philip Guin <philipguin@gmail.com>
2012-12-05 21:20:49 -0600

    Stuff, and stuff.

--- src/genome/GaussianRandomGenomeCreator.java
+++ src/genome/GaussianRandomGenomeCreator.java
@@ -8,14 +8,16 @@ public class GaussianRandomGenomeCreator implements ICreator<float[]>
 {
 	private final Random random;
 	private final int genomeLength;
-	private final float center, magnitude;
+	private final float floor, ceiling, center, multiplier;
 	
-	public GaussianRandomGenomeCreator(Random random, int genomeLength, float center, float magnitude)
+	public GaussianRandomGenomeCreator(Random random, int genomeLength, float floor, float ceiling, float center, float multiplier)
 	{
 		this.random = random;
 		this.genomeLength = genomeLength;
+		this.floor = floor;
+		this.ceiling = ceiling;
 		this.center = center;
-		this.magnitude = magnitude;
+		this.multiplier = multiplier;
 	}
 	
 	@Override
@@ -24,7 +26,7 @@ public class GaussianRandomGenomeCreator implements ICreator<float[]>
 		float[] result = new float[genomeLength];
 		
 		for (int i = 0; i < genomeLength; ++i)
-			result[i] = center + (float)random.nextGaussian() * magnitude;
+			result[i] = Math.max(floor, Math.min(ceiling, center + (float)random.nextGaussian() * multiplier));
 		
 		return result;
 	}
